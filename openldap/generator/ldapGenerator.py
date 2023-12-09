7384401eab13e6d17983bda92fb50904bfc766d4
MagnusFuruhaug <magnusefu@gmail.com>
2023-12-09 16:23:52 +0100

    Added faker library for more realistic data

--- openldap/generator/ldapGenerator.py
+++ openldap/generator/ldapGenerator.py
@@ -1,68 +1,98 @@
-import random
-import string
 import os
+import random
+from faker import Faker
 
-def generate_random_user(uid):
-    user = {
-        'dn': f"cn={uid},ou=Users,dc=example,dc=org",
-        'changetype': 'add',
-        'objectclass': ['inetOrgPerson'],
-        'cn': [uid],
-        'givenname': [f'GivenName_{uid}'],
-        'sn': [f'Surname_{uid}'],
-        'displayname': [f'DisplayName_{uid}'],
-        'mail': [f'{uid}@example.com'],
-        'uid': [uid],
-        'userpassword': [f'{uid}_pass']
-    }
-    return user
+fake = Faker()
 
-def generate_random_group(cn, unique_members):
-    group = {
-        'dn': f"cn={cn},ou=Groups,dc=example,dc=org",
-        'changetype': 'add',
-        'objectclass': ['organizationalUnit', 'groupOfUniqueNames'],
-        'cn': [cn],
-        'ou': [f'{cn}_OU'],
-        'uniqueMember': unique_members
-    }
-    return group
+def get_openldap_data_directory():
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    # Set the data directory path outside of the generator directory
+    data_directory = os.path.join(script_directory, "..", "data")
+    output_ldif_file = os.path.join(data_directory, "generated_data.ldif")
+    return output_ldif_file
 
-def generate_random_data(num_users, num_groups):
-    users = [generate_random_user(f'user{i}') for i in range(1, num_users + 1)]
-    groups = [
-        generate_random_group('Admins', [f'cn=admin_gh,ou=Users,dc=example,dc=org']),
-        generate_random_group('Maintainers', [f'cn=maintainer,ou=Users,dc=example,dc=org', f'cn=developer,ou=Users,dc=example,dc=org']),
-    ]
-    return users, groups
+def generate_group_entry():
+    ldif_content = "dn: ou=Groups,dc=example,dc=org\n"
+    ldif_content += "changetype: add\n"
+    ldif_content += "objectclass: organizationalUnit\n"
+    ldif_content += "ou: Groups\n\n"
+    return ldif_content
 
-def write_ldif_file(entries, output_file):
-    with open(output_file, 'w') as ldif_file:
-        for entry in entries:
-            ldif_file.write(f"dn: {entry['dn'][0]}\n")
-            ldif_file.write(f"changetype: {entry['changetype']}\n")
-            for key, values in entry.items():
-                if key not in ['dn', 'changetype']:
-                    for value in values:
-                        ldif_file.write(f"{key}: {value}\n")
-            ldif_file.write("\n")
+def generate_group_ldif(group_name, user_dn_list):
+    ldif_content = f"dn: cn={group_name},ou=Groups,dc=example,dc=org\n"
+    ldif_content += "changetype: add\n"
+    ldif_content += f"cn: {group_name}\n"
+    ldif_content += "objectclass: groupOfUniqueNames\n"
 
-# Get the absolute path of the current script
-script_directory = os.path.dirname(os.path.abspath(__file__))
+    # Determine a random subset size for the group
+    subset_size = random.randint(1, len(user_dn_list))
+    
+    # Shuffle the user_dn_list and select a random subset
+    random.shuffle(user_dn_list)
+    selected_members = user_dn_list[:subset_size]
 
-# Set the data directory path outside of the generator directory
-data_directory = os.path.join(script_directory, "..", "data")
+    # Generate uniqueMember entries for the group based on the subset
+    for user_dn in selected_members:
+        ldif_content += f"uniqueMember: {user_dn}\n"
 
-# Ensure the data directory exists
-os.makedirs(data_directory, exist_ok=True)
+    ldif_content += "\n"  # Add one line of space after the group entry
+    return ldif_content
 
-# Example usage:
-num_users = 3
-num_groups = 2
-output_ldif_file = os.path.join(data_directory, "generated_data.ldif")
+def generate_user_ldif(user_name):
 
-users_data, groups_data = generate_random_data(num_users, num_groups)
-all_entries = users_data + groups_data
-write_ldif_file(all_entries, output_ldif_file)
+    name = fake.name()
+    firstName = name.split(" ")[0]
+    lastName = name.split(" ")[1]
 
-print(f"LDIF file '{output_ldif_file}' generated successfully.")
+    user_dn = f"cn={user_name},dc=example,dc=org"
+    ldif_content = f"dn: {user_dn}\n"
+    ldif_content += "changetype: add\n"
+    ldif_content += "objectclass: inetOrgPerson\n"
+    ldif_content += f"cn: {user_name}\n"
+    ldif_content += f"givenname: {firstName}\n"  # First name
+    ldif_content += f"sn: {lastName}\n"  # Last name
+    ldif_content += f"displayname: {name}\n"  # Full name
+    ldif_content += f"mail: {fake.email()}\n"  # Email
+    ldif_content += f"uid: {fake.user_name()}\n"
+    ldif_content += f"userpassword: {fake.password()}\n"
+    ldif_content += f"o: {fake.company()}\n"  # Organization
+    ldif_content += f"title: {fake.job()}\n"  # Title
+    ldif_content += f"street: {fake.street_address()}\n"  # Street Address
+
+    ldif_content += "\n"  # Add one line of space after the user entry
+    return ldif_content, user_dn
+
+def generate_ldif_file(num_groups, num_users):
+    ldif_content = ""
+
+    user_dn_list = []
+
+    # Generate LDIF entries for users
+    for i in range(1, num_users + 1):
+        user_name = f"user{i}"
+        user_ldif, user_dn = generate_user_ldif(user_name)
+        ldif_content += user_ldif
+        user_dn_list.append(user_dn)
+
+    ldif_content += generate_group_entry()
+
+    # Generate LDIF entries for groups
+    for i in range(1, num_groups + 1):
+        if i != 1:  # Skip the first group entry (specified group entry)
+            group_name = f"Group{i}"
+            ldif_content += generate_group_ldif(group_name, user_dn_list)
+
+    return ldif_content
+
+# Get user input for the number of groups and users
+num_groups = int(input("Enter the number of groups: "))
+num_users = int(input("Enter the number of users: "))
+
+# Generate LDIF content
+ldif_content = generate_ldif_file(num_groups, num_users)
+
+# Save LDIF content to a file
+with open(get_openldap_data_directory(), "w") as ldif_file:
+    ldif_file.write(ldif_content)
+
+print("LDIF file generated successfully.")
